e63a1393792fd1304d0213b4912d2baf531e9048
leo-matheus <42416578+LeoMatheus@users.noreply.github.com>
2020-09-22 06:35:05 -0300

    Add files via upload

---
+++ relogio.js
@@ -0,0 +1,25 @@
+
+
+function horario() {
+    var hoje = new Date();
+    var h = hoje.getHours();
+    var m = hoje.getMinutes();
+    var s = hoje.getSeconds();
+
+    function checarTempo(i) {
+        if (i < 10) {
+            i = '0' + i
+        }
+        return i;
+    }
+    
+    s = checarTempo(s);
+    m = checarTempo(m);
+
+    document.getElementById('horas').innerHTML = `${h} : ${m} : ${s}`
+
+    setTimeout(horario, 1000);
+
+}
+
+horario()
